b094dbb68aa90eb0d6bca7be15065f2d11ec8e8c
Andres <af.rodriguezm1@uniandes.edu.co>
2018-10-18 15:58:04 -0500

    pagina con titulo Signed-off-by:Andres <af.rodriguezm1@uniandes.edu.co>

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,6 +3,9 @@ import {CommonModule} from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
 import {CriticoListComponent} from '../app/critico/critico-list/critico-list.component';
+import {FuncionListComponent} from '../app/funcion/funcion-list/funcion-list.component';
+import {TeatroListComponent} from '../app/teatro/teatro-list/teatro-list.component';
+import {UsuarioListComponent} from '../app/usuario/usuario-list/usuario-list.component';
 
 const routes: Routes = [
     {
@@ -11,6 +14,33 @@ const routes: Routes = [
             {
                 path: 'list',
                 component:CriticoListComponent
+            }
+        ]
+    },
+    {
+        path: 'funciones',
+        children: [
+            {
+                path: 'list',
+                component:FuncionListComponent
+            }
+        ]
+    },
+    {
+        path: 'teatros',
+        children: [
+            {
+                path: 'list',
+                component:TeatroListComponent
+            }
+        ]
+    },
+    {
+        path: 'usuarios',
+        children: [
+            {
+                path: 'list',
+                component:UsuarioListComponent
             }
         ]
     }
