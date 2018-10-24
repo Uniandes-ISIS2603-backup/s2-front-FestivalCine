a743c33b6c3e3893cd8e83544234504e81380100
Paula Velandia <pa.velandiar@uniandes.edu.co>
2018-10-24 18:16:21 -0500

    Arreglo environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-  apiURL: 'http://localhost:8080/s2_tarantino-api/api' 
+  production: false,
+  apiURL: 'http://localhost:8080/s2_festivalcine-api/'
 };
 
 /*
